7ec9e0f413635060fb39b6e9cf321e862716183e
Anurag Singh Bisht <anuragsinghbisht@outlook.com>
2017-07-12 22:44:45 +0530

    Setup ci (#13)
    * Add scripts to initialize commitizen and commit
    
    * feat: Add script to streamline conventions for writing commit messages

---
+++ app/actions/__tests__/index.action.test.js
@@ -0,0 +1,19 @@
+import * as actions from "../index.action";
+import { store } from "../../helper/test.helper";
+import actionTypes from "../actionTypes";
+
+describe("action", () => {
+  afterEach(() => {
+    store.clearActions();
+  });
+
+  it("should dispatch action", () => {
+    store.dispatch(actions.action());
+    expect(store.getActions()).toEqual([
+      {
+        type: actionTypes.ACTION,
+        payload: "hello"
+      }
+    ]);
+  });
+});
